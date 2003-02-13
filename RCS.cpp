ca74c8e1adecc4aa99740eeeee86d4a8a8c79a84
Jason Weathered <git@jason.backtick.org>
2003-02-13 13:00:27 +0000

    *** empty log message ***

--- RCS.cpp
+++ RCS.cpp
@@ -4,12 +4,12 @@
 #include "wx/wxprec.h"
 
 #include "RCS.h"
-RCS_ID($Id: RCS.cpp,v 1.1 2003-02-13 12:51:35 jason Exp $)
+RCS_ID($Id: RCS.cpp,v 1.2 2003-02-13 13:00:27 jason Exp $)
 
-int add_rcs_id(const char *id)
+int add_rcs_id(const char *x, const char *y)
 {
 	wxString str;
-	str << "\n" << id << "\n";
+	str << "\n" << x << "\n" << y << "\n";
 	OutputDebugString(str.c_str());
 	return 0;
 }
--- RCS.h
+++ RCS.h
@@ -1,8 +1,8 @@
 #ifndef RCS_H_
 #define RCS_H_
 
-int add_rcs_id(const char *rcs_id);
+int add_rcs_id(const char *x, const char *y);
 
-#define RCS_ID(id) static int rcs_id_ ## wxMAKE_UNIQUE_ASSERT_NAME = add_rcs_id( #id );
+#define RCS_ID(x,y) static int rcs_id_ ## wxMAKE_UNIQUE_ASSERT_NAME = add_rcs_id( #x, #y );
 
 #endif
