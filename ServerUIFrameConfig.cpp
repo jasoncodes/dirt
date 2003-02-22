941d93d282d3dd07e957038ee02c76b8d916b0c9
Jason Weathered <git@jason.backtick.org>
2003-02-22 03:24:26 +0000

    *** empty log message ***

--- ServerUIFrameConfig.cpp
+++ ServerUIFrameConfig.cpp
@@ -6,7 +6,7 @@
 	#include "wx/wx.h"
 #endif
 #include "RCS.h"
-RCS_ID($Id: ServerUIFrameConfig.cpp,v 1.3 2003-02-22 03:21:04 jason Exp $)
+RCS_ID($Id: ServerUIFrameConfig.cpp,v 1.4 2003-02-22 03:24:26 jason Exp $)
 
 #include "ServerUIFrameConfig.h"
 
@@ -38,45 +38,48 @@ ServerUIFrameConfig::ServerUIFrameConfig(ServerUIFrame *parent, Server *server)
 	wxTextCtrl *txtAdminPass = new wxTextCtrl(panel, -1, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_PASSWORD);
 	FixBorder(txtAdminPass);
 
-	wxBoxSizer *szrRight = new wxBoxSizer(wxVERTICAL);
-	{
-		szrRight->Add(cmdOK, 0, wxTOP | wxBOTTOM | wxEXPAND, 8);
-		szrRight->Add(cmdCancel, 0, wxBOTTOM | wxEXPAND, 8);
-		szrRight->Add(cmdApply, 0, wxBOTTOM | wxEXPAND, 8);
-	}
-
-	wxBoxSizer *szrLabels = new wxBoxSizer(wxVERTICAL);
-	{
-		szrLabels->Add(lblUserPass, 1, wxRIGHT, 8);
-		szrLabels->Add(lblAdminPass, 1, wxRIGHT, 8);
-	}
-
-	wxBoxSizer *szrTextBoxes = new wxBoxSizer(wxVERTICAL);
-	{
-		szrTextBoxes->Add(txtUserPass, 1, wxBOTTOM | wxEXPAND, 8);
-		szrTextBoxes->Add(txtAdminPass, 1, wxBOTTOM | wxEXPAND, 8);
-	}
-
-	wxBoxSizer *szrLeftTop = new wxBoxSizer(wxHORIZONTAL);
-	{
-		szrLeftTop->Add(szrLabels, 0, wxEXPAND, 0);
-		szrLeftTop->Add(szrTextBoxes, 1, wxEXPAND, 0);
-	}
-	
-	wxBoxSizer *szrLeftFill = new wxBoxSizer(wxHORIZONTAL);
-	{
-	}
-
-	wxBoxSizer *szrLeft = new wxBoxSizer(wxVERTICAL);
-	{
-		szrLeft->Add(szrLeftTop, 0, wxEXPAND, 0);
-		szrLeft->Add(szrLeftFill, 1, wxEXPAND, 0);
-	}
-
 	wxBoxSizer *szrAll = new wxBoxSizer(wxHORIZONTAL);
 	{
+
+		wxBoxSizer *szrLeft = new wxBoxSizer(wxVERTICAL);
+		{
+
+			wxBoxSizer *szrLeftTop = new wxBoxSizer(wxHORIZONTAL);
+			{
+
+				wxBoxSizer *szrLabels = new wxBoxSizer(wxVERTICAL);
+				{
+					szrLabels->Add(lblUserPass, 1, wxRIGHT, 8);
+					szrLabels->Add(lblAdminPass, 1, wxRIGHT, 8);
+				}
+				szrLeftTop->Add(szrLabels, 0, wxEXPAND, 0);
+
+				wxBoxSizer *szrTextBoxes = new wxBoxSizer(wxVERTICAL);
+				{
+					szrTextBoxes->Add(txtUserPass, 1, wxBOTTOM | wxEXPAND, 8);
+					szrTextBoxes->Add(txtAdminPass, 1, wxBOTTOM | wxEXPAND, 8);
+				}
+				szrLeftTop->Add(szrTextBoxes, 1, wxEXPAND, 0);
+
+			}
+			szrLeft->Add(szrLeftTop, 0, wxEXPAND, 0);
+
+			wxBoxSizer *szrLeftFill = new wxBoxSizer(wxHORIZONTAL);
+			{
+			}
+			szrLeft->Add(szrLeftFill, 1, wxEXPAND, 0);
+
+		}
 		szrAll->Add(szrLeft, 1, wxLEFT | wxTOP | wxBOTTOM | wxEXPAND, 8);
+
+		wxBoxSizer *szrRight = new wxBoxSizer(wxVERTICAL);
+		{
+			szrRight->Add(cmdOK, 0, wxTOP | wxBOTTOM | wxEXPAND, 8);
+			szrRight->Add(cmdCancel, 0, wxBOTTOM | wxEXPAND, 8);
+			szrRight->Add(cmdApply, 0, wxBOTTOM | wxEXPAND, 8);
+		}
 		szrAll->Add(szrRight, 0, wxALL | wxEXPAND, 8);
+
 	}
 
 	LoadSettings();
