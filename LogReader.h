337896b7a45396f6dd3aa87b1b3af465daa02aed
Jason Weathered <git@jason.backtick.org>
2003-03-18 10:15:08 +0000

    *** empty log message ***

---
+++ LogReader.h
@@ -0,0 +1,49 @@
+#ifndef LogReader_H_
+#define LogReader_H_
+
+#include "Log.h"
+#include "ByteBuffer.h"
+#include "Crypt.h"
+#include "Util.h"
+
+class LogReader
+{
+
+public:
+	LogReader(const wxString &filename);
+	virtual ~LogReader();
+
+	virtual bool Ok() const;
+
+	virtual ByteBuffer GetPublicKey();
+	virtual void SetPrivateKey(const wxString &private_key);
+
+	virtual ByteBufferHashMap GetProperties() const;
+	virtual ByteBuffer GetProperty(const wxString &name) const;
+
+	virtual bool IsEof() const;
+	virtual void Reset();
+	virtual off_t GetPosition() const;
+	virtual off_t GetLength() const;
+
+	inline bool HasNext() const { return !IsEof(); }
+	virtual LogEntryType GetNext();
+
+	virtual wxString GetText();
+
+protected:
+	virtual void Init(const wxString &filename);
+	virtual ByteBuffer Read();
+
+protected:
+	wxFile m_file;
+	Crypt m_crypt;
+	ByteBuffer m_private_key;
+	ByteBuffer m_block_key;
+	ByteBufferHashMap m_properties;
+	ByteBuffer m_entry;
+	bool m_first_pass;
+
+};
+
+#endif
---
+++ LogWriter.h
@@ -0,0 +1,44 @@
+#ifndef LogWriter_H_
+#define LogWriter_H_
+
+#include "Log.h"
+#include "ByteBuffer.h"
+#include "Crypt.h"
+#include "Util.h"
+#include <wx/datetime.h>
+
+class LogWriter
+{
+
+public:
+	LogWriter(const wxString &filename);
+	LogWriter(const wxDateTime &date = wxDateTime::Now(), const wxString &name = wxEmptyString);
+	virtual ~LogWriter();
+
+	static wxString GenerateFilename(const wxDateTime &date = wxDateTime::Now(), const wxString &name = wxEmptyString);
+
+	virtual bool Ok() const;
+
+	virtual void SetPublicKey(const ByteBuffer &public_key);
+
+	virtual ByteBufferHashMap GetProperties() const;
+	virtual ByteBuffer GetProperty(const wxString &name) const;
+	virtual void SetProperty(const wxString &name, const ByteBuffer &value);
+
+	virtual void AddText(const wxString &line, const wxColour &line_colour = *wxBLACK, bool convert_urls = true);
+	virtual void AddSeparator();
+
+protected:
+	virtual void Init(const wxString &filename);
+	virtual void Write(const ByteBuffer &data);
+
+protected:
+	wxFile m_file;
+	Crypt m_crypt;
+	ByteBuffer m_public_key;
+	ByteBuffer m_block_key;
+	ByteBufferHashMap m_properties;
+
+};
+
+#endif
