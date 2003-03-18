fb91daa20dd1e109e0b5b53fcedb9d5bd42ef995
Jason Weathered <git@jason.backtick.org>
2003-03-18 10:21:07 +0000

    *** empty log message ***

---
+++ LogReader.cpp
@@ -0,0 +1,11 @@
+#include "wx/wxprec.h"
+#ifdef __BORLANDC__
+	#pragma hdrstop
+#endif
+#ifndef WX_PRECOMP
+	#include "wx/wx.h"
+#endif
+#include "RCS.h"
+RCS_ID($Id: LogReader.cpp,v 1.1 2003-03-18 10:21:07 jason Exp $)
+
+#include "LogReader.h"
