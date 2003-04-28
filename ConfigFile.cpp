71528dd44e90efbe762daac886fcc4d55992f194
Jason Weathered <git@jason.backtick.org>
2003-04-28 12:44:48 +0000

    *** empty log message ***

--- ConfigFile.cpp
+++ ConfigFile.cpp
@@ -6,15 +6,32 @@
 	#include "wx/wx.h"
 #endif
 #include "RCS.h"
-RCS_ID($Id: ConfigFile.cpp,v 1.1 2003-04-18 10:05:44 jason Exp $)
+RCS_ID($Id: ConfigFile.cpp,v 1.2 2003-04-28 12:44:48 jason Exp $)
 
 #include "ConfigFile.h"
 #include "Dirt.h"
+#include "util.h"
+#include <wx/filename.h>
 
 DECLARE_APP(DirtApp)
 
+static wxString GetConfigFilename()
+{
+	wxString filename = wxGetApp().GetConfigFilename();
+	if (filename.Length() == 0)
+	{
+		wxFileName fn(GetSelf());
+		fn.SetFullName(wxT("dirt.ini"));
+		if (fn.FileExists())
+		{
+			return fn.GetFullPath();
+		}
+	}
+	return filename;
+}
+
 ConfigFile::ConfigFile()
-	: wxFileConfig(wxT("dirt"), wxT(""), wxGetApp().GetConfigFilename())
+	: wxFileConfig(wxT("dirt"), wxT(""), GetConfigFilename())
 {
 	SetUmask(0077);
 }
