a6d361126738731bc73141358fb158f5e87f5375
Jason Weathered <git@jason.backtick.org>
2007-07-19 22:48:55 +0000

    *** empty log message ***

--- src/au/com/gslabs/dirt/Dirt.java
+++ src/au/com/gslabs/dirt/Dirt.java
@@ -3,9 +3,6 @@ package au.com.gslabs.dirt;
 import au.com.gslabs.dirt.lib.util.FileUtil;
 import au.com.gslabs.dirt.ui.UI;
 
-import au.com.gslabs.dirt.lib.util.ByteBuffer;
-import au.com.gslabs.dirt.lib.crypt.*;
-
 public class Dirt
 {
 	
@@ -13,21 +10,8 @@ public class Dirt
 	
 	public static void main(String[] args)
 	{
-		//FileUtil.ShutdownCleanupCheck(args);
-		//UI.startClient(args);
-		try
-		{
-			Crypt crypt = CryptFactory.getInstance();
-			KeyPair pair = crypt.generateKeyPair("RSA", 1024);
-			ByteBuffer plain = new ByteBuffer("This is a test.");
-			ByteBuffer encrypted = crypt.encrypt("RSA", pair.getPublicKey(), plain);
-			ByteBuffer decrypted = crypt.decrypt("RSA", pair.getPrivateKey(), encrypted);
-			System.out.println(decrypted);
-		}
-		catch (CryptException ex)
-		{
-			ex.printStackTrace();
-		}
+		FileUtil.ShutdownCleanupCheck(args);
+		UI.startClient(args);
 	}
 	
 }
