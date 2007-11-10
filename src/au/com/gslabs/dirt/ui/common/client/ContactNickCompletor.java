1108fd6db462a8f8af592d8a508fb0a8ba723497
Jason Weathered <git@jason.backtick.org>
2007-11-10 07:02:09 +0000

    *** empty log message ***

--- src/au/com/gslabs/dirt/ui/common/client/ContactNickCompletor.java
+++ src/au/com/gslabs/dirt/ui/common/client/ContactNickCompletor.java
@@ -33,21 +33,42 @@ public class ContactNickCompletor implements au.com.gslabs.dirt.lib.ui.common.Co
 		
 		String toFind = (buffer == null) ? "" : buffer;
 		toFind = toFind.substring(0, cursor);
-		int idxSpace = toFind.lastIndexOf(' ');
+		final int idxSpace = toFind.lastIndexOf(' ');
 		if (idxSpace > -1)
 		{
 			toFind = toFind.substring(idxSpace+1);
 		}
-		toFind = toFind.toLowerCase();
+		final String toFindLower = toFind.toLowerCase();
 		
+		int countCaseMatches = 0;
+		
+		// find nicks which match the prefix (case insensitive)
 		for (Contact contact : client.getContacts().values())
 		{
 			Duration offlineDuration = contact.getOfflineDuration();
 			if (offlineDuration == null || offlineDuration.getMilliseconds() <= OFFLINE_GRACE_MILLISECONDS)
 			{
-				if (contact.getNickname().toLowerCase().startsWith(toFind))
+				final String thisNick = contact.getNickname();
+				if (thisNick.toLowerCase().startsWith(toFindLower))
 				{
 					candidates.add(contact.getNickname());
+					if (thisNick.startsWith(toFind))
+					{
+						countCaseMatches++;
+					}
+				}
+			}
+		}
+		
+		// if at least one match had the same case as the entered prefix
+		// we switch into case sensitive mode by removing all other matches
+		if (countCaseMatches > 0 && countCaseMatches < candidates.size())
+		{
+			for (int i = candidates.size()-1; i >= 0; --i)
+			{
+				if (!candidates.get(i).startsWith(toFind))
+				{
+					candidates.remove(i);
 				}
 			}
 		}
