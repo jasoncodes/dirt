fd3ef84c411aae4fdc5d9d316b7c0049f6947829
Jason Weathered <git@jason.backtick.org>
2008-01-18 09:04:17 +0000

    *** empty log message ***

--- run.cmd
+++ run.cmd
@@ -1,2 +1,5 @@
-@set PATH=%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin;%PATH%
+@set DIRT_PATH_OLD=%PATH%
+@set PATH=%PATH%;%JAVA_HOME%\bin;%JAVA_HOME%\jre\bin
 java -jar dist/Dirt.jar
+@set PATH=%DIRT_PATH_OLD%
+@set DIRT_PATH_OLD=
